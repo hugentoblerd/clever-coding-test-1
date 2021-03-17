df373d061ab3853dae95aca951b681256e51f600
David Hugentobler <davidhugentobler30@gmail.com>
2021-03-17 16:41:11 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <NumberSelector :numbers="numbers" :selectedNumber="selectedNumber"/>
+  </div>
+</template>
+
+<script>
+import NumberSelector from './components/NumberSelector'
+
+export default {
+  name: 'App',
+  components: {
+    NumberSelector
+  },
+  data () {
+    return {
+      numbers: [],
+      selectedNumber: false
+    }
+  },
+  created: function () {
+    let num = 1
+
+    for (let i = 0; i < 100; i++) {
+      this.numbers = num++
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/NumberSelector.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="hello">
+    <h1 v-if="selectedNumber">The selected number is {{ selectedNumber }}</h1>
+    <h1 v-else>None Selected</h1>
+    <div class="container">
+        <div class="content" v-for="number in numbers" :key="number">
+            <button v-on:click="selectedNumber = number">{{ number }}</button>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['numbers', 'selectedNumber']
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.container {
+    display: flex;
+    flex-wrap: wrap;
+}
+.content {
+    width: 50px;
+}
+</style>
